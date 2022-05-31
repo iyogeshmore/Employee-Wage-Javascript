28108c73f6be53782dc6b4ef01cdf91366633230
Yogesh More <iyogesh.more1998@gmail.com>
2022-05-31 20:09:09 +0530

    Added : Refactored code to write function for daily working Hours.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,5 +1,4 @@
-// UC2 :Calculating Employee Wage
-
+//UC 3 : Refactored code to write function for daily working Hours
 const IS_ABSENT = 0;
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
@@ -7,23 +6,20 @@ const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
 const WAGE_PER_HOUR = 20;
 
+function getWorkingHours(empCheck){
+    switch (empCheck){
+    case IS_ABSENT:
+    return 0;
+
+    case IS_FULL_TIME:
+    return FULL_TIME_HOURS;
+
+    case IS_PART_TIME:
+    return PART_TIME_HOURS;
+    }
+}
 let empHours = 0;
 let empCheck = Math.floor(Math.random()*10)%3;
-
-switch (empCheck){
-    case IS_ABSENT:
-    empHours = 0
-    console.log("Employee is ABSENT.");
-    break;
-    
-    case IS_FULL_TIME:
-    empHours = FULL_TIME_HOURS;
-    break;
-    
-    case IS_PART_TIME:
-    empHours = PART_TIME_HOURS;
-    break;
-}
-
+empHours = getWorkingHours(empCheck);
 let empWage = empHours * WAGE_PER_HOUR;
 console.log("Employee wage: "+ empWage);
