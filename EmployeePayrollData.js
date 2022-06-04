cfb6a622fa4e6faa4265d03b1ced12b7f5cf3a4a
Yogesh More <iyogesh.more1998@gmail.com>
2022-06-04 13:26:35 +0530

    Added : Ability to extend Employee Payroll Data to store gender and start date .

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,13 +1,29 @@
+// UC 12 - Extend Employee Payroll Class
+
 class EmployeePayrollData{
     id;
     salary;
-
-    constructor(id, name, salary){   // constructor
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    gender;
+    startDate;
+ 
+    constructor(...params){       // constructor
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
-    get name(){                     // Getter Setter Method
+
+    get id(){         // Getter Setter Method
+
+        return this._id;
+    }
+
+    set id(id){
+        this._id = id;
+    }
+
+    get name(){
         return this._name;
     }
 
@@ -15,8 +31,36 @@ class EmployeePayrollData{
         this._name = name;
     }
 
-    toString(){                     // Method
-        return "ID: "+ this.id + ", Name: "+ this.name + ", Salary: "+ this.salary;
+    get salary(){
+        return this._salary;
+    }
+
+    set salary(salary){
+        this._salary = salary;
+    }
+
+    get gender(){
+        return this.gender;
+    }
+
+    set gender(gender){
+        this._gender = gender;
+    }
+
+    get startDate(){
+        return this.startDate;
+    }
+
+    set startDate(startDate){
+        this._startDate = startDate;
+    }
+
+    toString(){     // Method
+        const options = {year: 'numeric', month: 'long', day: 'numeric'};
+        const empDate = this.startDate === undefined ? "undefined" :
+                        this.startDate.toLocaleDateString("en-US", options);
+        return "ID: "+ this.id + ", Name: "+ this.name + ", Salary: "+ this.salary+
+                ", Gender: "+ this.gender + ", Start Date: "+empDate;
     }
 }
 
@@ -24,3 +68,6 @@ let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "John"
 console.log(employeePayrollData.toString());
+
+let newEmployeePayrollData = new EmployeePayrollData(1, "Terissa", 40000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
